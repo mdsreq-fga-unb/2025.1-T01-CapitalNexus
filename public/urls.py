68c7c766169093264e3b7f8005d3800c179984b5
Sophiassilva <sophiassilva.901@gmail.com>
2025-06-23 16:52:30 -0300

    feat(site): configura mensagens de erro ou confirmação de envio

--- public/urls.py
+++ public/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path('', views.sobre, name='sobre'),
     path('contato/', views.pagina_contato, name='contato'),
     path('projetos/', views.pagina_projetos, name='projetos'),
-    path('sucesso/', views.pagina_sucesso, name='sucesso'),
 ]
