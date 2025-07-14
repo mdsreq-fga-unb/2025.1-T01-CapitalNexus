036a06263d5ebe013ab1f3458da191ce160e9e21
Sophia Silva <162454522+Sophiassilva@users.noreply.github.com>
2025-07-14 20:23:57 -0300

    Update urls.py

--- capital_nexus/urls.py
+++ capital_nexus/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('public/', include('public.urls')),
+    path('', include('public.urls')),
     path('members/', include('members.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
